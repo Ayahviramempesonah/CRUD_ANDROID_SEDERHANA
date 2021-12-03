085a916513a34c3591c24facf3ed4eb612cf6acc
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2021-12-03 23:24:07 +0700

    penambahan sedikit logic di buatFile

--- app/src/main/java/com/ex/MainActivity.java
+++ app/src/main/java/com/ex/MainActivity.java
@@ -1,13 +1,98 @@
 package com.ex;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    public static final String FILENAME="namaFile.txt";
+    Button buatFile,bacaFile,ubahFile,hapusFile;
+    TextView textView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buatFile=findViewById(R.id.btn_create);
+        bacaFile=findViewById(R.id.btn_baca);
+        ubahFile=findViewById(R.id.btn_ubah);
+        hapusFile=findViewById(R.id.btn_hapus);
+        textView=findViewById(R.id.ini_textView);
+
+
+        buatFile.setOnClickListener(this);
+        bacaFile.setOnClickListener(this);
+        ubahFile.setOnClickListener(this);
+        hapusFile.setOnClickListener(this);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    void buatFile(){
+        String isiFile="coba isi data file txt";
+        File file = new File(getFilesDir(),FILENAME);
+
+        FileOutputStream fileOutputStream=null;
+        try {
+            file.createNewFile();
+            fileOutputStream = new FileOutputStream(file,true);
+            fileOutputStream.write(isiFile.getBytes());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
